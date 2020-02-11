d6359e1ca43ea02df59fad1c1786f30b249e2fe9
Peter Sanford <psanford@slack-corp.com>
2020-02-11 10:17:56 -0800

    sudoParser: properly parse ENV variables
    Running a command: `sudo FOO=1 /some/cmd` will cause sudo to log the
    environment variable FOO. We previously were incorrectly capturing
    that as part of the 'as_user' field. We now properly capture the ENV
    field.

--- test/parsers/sudoParser.test.js
+++ test/parsers/sudoParser.test.js
@@ -34,4 +34,36 @@ describe('sudoParser', function () {
         )
     })
 
+    it('Should parse ENV variables properly', function () {
+        assertParserResult(
+            StreamStash.parsers.sudoParser.raw,
+            '    ubuntu : TTY=pts/0 ; PWD=/home/ubuntu ; USER=root ; ENV=FOO=1 BAR=2 ; COMMAND=/bin/ls -l',
+            {
+                as_user: 'root',
+                command: '/bin/ls -l',
+                event: 'command',
+                pwd: '/home/ubuntu',
+                env: 'FOO=1 BAR=2',
+                tty: 'pts/0',
+                user: 'ubuntu'
+            }
+        )
+    })
+
+    it('Should parse ENV variables properly in the error case', function () {
+        assertParserResult(
+            StreamStash.parsers.sudoParser.raw,
+            '    ubuntu : command not allowed ; TTY=pts/0 ; PWD=/home/ubuntu ; USER=root ; ENV=FOO=1 BAR=2 ; COMMAND=/bin/ls -l',
+            {
+                as_user: 'root',
+                command: '/bin/ls -l',
+                error: 'command not allowed',
+                event: 'error',
+                pwd: '/home/ubuntu',
+                env: 'FOO=1 BAR=2',
+                tty: 'pts/0',
+                user: 'ubuntu'
+            }
+        )
+    })
 })
